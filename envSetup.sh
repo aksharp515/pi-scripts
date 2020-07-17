43f2a4be3c217eed7956264bcca107a7a9831d2e
Akshar Patel <apatel@posteo.net>
2020-07-17 18:48:00 -0400

    added disabling killswitch and killing openvpn within script

--- envSetup.sh
+++ envSetup.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
+sudo killall openvpn
+sleep 5 
+
+sudo ufw disable
+sleep 20
+
 UDP_HOME=etc/openvpn
 SERVER_EXT=nordvpn.com.udp1194.ovpn
+KILLSWITCHFILE=/home/pi/scripts/autoKillSwitch.sh
+LOGFILE=/home/pi/logs/vpnAddresses.txt
+DATE=$(date)
 
 SERVER_LINE=`cat /$UDP_HOME/us$1.$SERVER_EXT | grep remote -m 1 | grep -oP "(?<=remote )[^ ]+"`
 
-DATE=$(date)
-echo >> ~/logs/vpnAddresses.txt
-echo $DATE >> ~/logs/vpnAddresses.txt
-echo Server Address $SERVER_LINE >> ~/logs/vpnAddresses.txt
-echo Server# $1 >> ~/logs/vpnAddresses.txt
+echo >> "$LOGFILE"
+echo $DATE >> "$LOGFILE"
+echo Server Address $SERVER_LINE >> "$LOGFILE"
+echo Server# $1 >> "$LOGFILE"
 
-echo "sudo ufw reset" > /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw allow in to 192.168.1.0/24" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw allow out to 192.168.1.0/24" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw default deny outgoing" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw default deny incoming" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw allow out on tun0 from any to any" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw allow out to $SERVER_LINE port 1194 proto udp" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw allow in on tun0 from any to any" >> /home/pi/scripts/autoKillSwitch.sh
-echo "sudo ufw enable" >> /home/pi/scripts/autoKillSwitch.sh
+echo "sudo ufw reset" > "$KILLSWITCHFILE"
+echo "sudo ufw allow in to 192.168.1.0/24" >> "$KILLSWITCHFILE"
+echo "sudo ufw allow out to 192.168.1.0/24" >> "$KILLSWITCHFILE"
+echo "sudo ufw default deny outgoing" >> "$KILLSWITCHFILE"
+echo "sudo ufw default deny incoming" >> "$KILLSWITCHFILE"
+echo "sudo ufw allow out on tun0 from any to any" >> "$KILLSWITCHFILE"
+echo "sudo ufw allow out to $SERVER_LINE port 1194 proto udp" >> "$KILLSWITCHFILE"
+echo "sudo ufw allow in on tun0 from any to any" >> "$KILLSWITCHFILE"
+echo "sudo ufw enable" >> "$KILLSWITCHFILE"
 
-chmod 777 /home/pi/scripts/autoKillSwitch.sh 
+chmod 777 "$KILLSWITCHFILE"
 
-sudo openvpn /$UDP_HOME/us$1.$SERVER_EXT
+sudo openvpn /$UDP_HOME/us$1.$SERVER_EXT &
 
+sleep 10
 
+yes | "$KILLSWITCHFILE" &
